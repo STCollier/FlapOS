25f4a122156769834c7a73b043f6dc67c2204370
STCollier <1samuel3collier@gmail.com>
2025-08-21 16:40:19 -0400

    Enable A20 line

--- src/boot.asm
+++ src/boot.asm
@@ -45,6 +45,63 @@
     mov al, 0x13
     int 0x10
 
+.check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+
+    cli
+
+    xor ax, ax ; ax = 0
+    mov es, ax
+
+    not ax ; ax = 0xFFFF
+    mov ds, ax
+
+    mov di, 0x0500
+    mov si, 0x0510
+
+    mov al, byte [es:di]
+    push ax
+
+    mov al, byte [ds:si]
+    push ax
+
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+
+    cmp byte [es:di], 0xFF
+
+    pop ax
+    mov byte [ds:si], al
+
+    pop ax
+    mov byte [es:di], al
+
+    mov ax, 0
+    je .check_a20__exit
+
+    mov ax, 1
+
+.check_a20__exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+
+    cmp ax, 0
+    je .enable_a20
+
+    jmp .draw
+
+.enable_a20:
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
 .draw:
     cmp si, 320
     je .done
